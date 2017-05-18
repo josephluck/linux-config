8c8d95381da026e018151ea80a1fe4702c0a2bc5
Joseph Luck <joseph.luck@sky.com>
2017-05-18 22:43:16 +0100

    Add gtk & icon themes

--- install.sh
+++ install.sh
@@ -16,11 +16,21 @@ curl -sL https://deb.nodesource.com/setup_7.x | sudo -E bash - &&
 # sur5r-keyring
 sudo apt-get --allow-unauthenticated install sur5r-keyring &&
 
+# Arc gtk theme
+sudo sh -c "echo 'deb http://download.opensuse.org/repositories/home:/Horst3180/xUbuntu_16.04/ /' >> /etc/apt/sources.list.d/arc-theme.list" &&
+wget http://download.opensuse.org/repositories/home:Horst3180/xUbuntu_16.04/Release.key &&
+sudo apt-key add - < Release.key &&
+rm -rf Release.key &&
+
+# Moka icons
+sudo add-apt-repository ppa:moka/daily &&
+
+
 # Update repositories
 sudo apt-get update &&
 
 # Install packages
-sudo apt-get install -y nodejs git code i3 gksu amixer pulseaudio powertop feh arandr python-requests &&
+sudo apt-get install -y nodejs git code i3 gksu amixer pulseaudio powertop feh arandr python-requests lxappearance arc-theme moka-icon-theme &&
 sudo apt-get install gcc python-dev python-pip &&
 pip install netifaces && pip install psutil
 
@@ -33,7 +43,7 @@ rm -rf ~/.i3 && mkdir ~/.i3 && cp ./config ~/.i3/config &&
 
 # Bumblebee status
 git clone git://github.com/tobi-wan-kenobi/bumblebee-status &&
-cp -rf ./bumblebee-status ./.i3 && rm -rf ./bumblebee-status
+cp -rf ./bumblebee-status ~/.i3/bumblebee-status && rm -rf ./bumblebee-status
 
 # Copy media key scripts to local bin
 cp ./bright /usr/local/bin/bright &&
