174ac96f5214b7397224d7a3dd74062fa19dcbd4
Joseph Luck <josephluck@users.noreply.github.com>
2017-05-27 20:47:23 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -15,7 +15,6 @@ sudo apt-get install curl &&
 sudo apt-get upgrade &&
 
 # Install packages
-sudo apt-get install -y i3 &&
 sudo apt-get install -y chromium-browser &&
 sudo apt-get install -y thunar &&
 sudo apt-get install -y gksu &&
@@ -32,14 +31,18 @@ sudo apt-get install -y xfce4-volumed &&
 curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg &&
 sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg &&
 sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list' &&
+sudo apt-get update &&
 sudo apt-get install -y code &&
 
 # Node
 curl -sL https://deb.nodesource.com/setup_7.x | sudo -E bash - &&
+sudo apt-get update &&
 sudo apt-get install -y nodejs &&
 
-# sur5r-keyring
+# i3 / sur5r-keyring
 sudo apt-get --allow-unauthenticated install sur5r-keyring &&
+sudo apt-get update &&
+sudo apt-get install -y i3 &&
 
 # Arc gtk theme
 sudo sh -c "echo 'deb http://download.opensuse.org/repositories/home:/Horst3180/xUbuntu_16.04/ /' >> /etc/apt/sources.list.d/arc-theme.list" &&
@@ -51,18 +54,22 @@ rm -rf Release.key &&
 # Spotify
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys BBEBDCB318AD50EC6865090613B00F1FD2C19886 &&
 echo deb http://repository.spotify.com stable non-free | sudo tee /etc/apt/sources.list.d/spotify.list &&
+sudo apt-get update &&
 sudo apt-get install -y spotify-client &&
 
 # Pick (color picker)
 sudo add-apt-repository ppa:sil/pick &&
+sudo apt-get update &&
 sudo apt-get install -y pick-colour-picker &&
 
 # Compton
 sudo apt-add-repository ppa:richardgv/compton &&
+sudo apt-get update &&
 sudo apt-get install -y compton &&
 
 # Font rendering
 # sudo add-apt-repository ppa:no1wantdthisname/ppa &&
+# sudo apt-get update &&
 # sudo apt-get install -y fontconfig-infinality &&
 
 # Bumblebee status
@@ -77,6 +84,7 @@ rm Monaco_Linux.ttf &&
 
 # oh my zsh
 sudo apt-get install -y zsh &&
+sudo apt-get update &&
 wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh &&
 
 # Clean up
