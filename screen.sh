006ef1312645752974556b58aafe0d7ab9c83cbe
Joseph Luck <josephluck@users.noreply.github.com>
2017-10-10 13:57:36 +0100

    Update screen.sh

--- screen.sh
+++ screen.sh
@@ -1,1 +1,2 @@
-xrandr --output HDMI-2 --off --output HDMI-1 --off --output DP-1 --mode 2560x1440 --pos 0x0 --rotate normal --output eDP-1 --primary --mode 2560x1600 --pos 0x1440 --rotate normal --output DP-2 --off
+#!/bin/sh
+xrandr --output HDMI-2 --off --output HDMI-1 --off --output DP-1 --mode 2560x1440 --pos 0x0 --rotate normal --output eDP-1 --primary --mode 1920x1200 --pos 304x1440 --rotate normal --output DP-2 --off
